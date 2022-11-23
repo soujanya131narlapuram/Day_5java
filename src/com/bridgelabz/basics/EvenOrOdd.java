038c31e07d950aeac226cbbda34ea4bb21d40a9d
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-11-23 19:19:51 +0530

    evenorodd branch upload

---
+++ src/com/bridgelabz/basics/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.basics;
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number :");
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("even number");
+        } else {
+            System.out.println("odd number");
+        }
+    }
+}
